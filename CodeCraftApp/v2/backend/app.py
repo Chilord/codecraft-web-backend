02932465f61ccd549b4c2b0c2b57f1555acdacc6
Chilord <143878017+Chilord@users.noreply.github.com>
2026-04-25 08:03:44 +0100

    Restrict CORS origins to specific URL

--- CodeCraftApp/v2/backend/app.py
+++ CodeCraftApp/v2/backend/app.py
@@ -4,7 +4,7 @@ from .routes.router import auth_bp
 from datetime import timedelta
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, origins=["https://codecraft-studio.surge.sh"])
 app.secret_key = "i}UuBu_buG_BuHbh_ufIyu(f67*yfityf/diTurD67{t]ojbur.fdc6rdDufufFyfyf)fYutdydfI"  # change later
 app.permanent_session_lifetime = timedelta(days=30)
 app.register_blueprint(auth_bp)
